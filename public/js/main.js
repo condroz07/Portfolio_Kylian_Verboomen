292092f22262b676eb87fbefc297c7542c5f2b02
condroz07 <kylianverboomen2001@gmail.com>
2022-10-31 23:55:13 +0100

    port

--- public/js/main.js
+++ public/js/main.js
@@ -1,41 +1,37 @@
-//Smooth Scroll
 SmoothScroll({
-    // Scrolling Core
-    animationTime    : 400, // [ms]
-    stepSize         : 100, // [px]
- 
-    // Acceleration
-    accelerationDelta : 50,  // 50
-    accelerationMax   : 3,   // 3
- 
-    // Keyboard Settings
-    keyboardSupport   : true,  // option
-    arrowScroll       : 50,    // [px]
- 
-    // Pulse (less tweakable)
-    // ratio of "tail" to "acceleration"
-    pulseAlgorithm   : true,
-    pulseScale       : 4,
-    pulseNormalize   : 1,
- 
-    // Other
-    touchpadSupport   : false, // ignore touchpad by default
-    fixedBackground   : true, 
-    excluded          : ''    
- });
- 
- 
- // Change style of navbar on scroll
- window.onscroll = function() {
+  // Scrolling Core
+  animationTime: 400,
+  stepSize: 100,
+
+  // Acceleration
+  accelerationDelta: 50,
+  accelerationMax: 3,
+
+  // Keyboard Settings
+  keyboardSupport: true,
+  arrowScroll: 50,
+
+  pulseAlgorithm: true,
+  pulseScale: 4,
+  pulseNormalize: 1,
+
+  // Other
+  touchpadSupport: false,
+  fixedBackground: true,
+  excluded: ''
+});
+
+
+// Change style of navbar on scroll
+window.onscroll = function () {
   myFunction()
- };
- 
- function myFunction() {
+};
+
+function myFunction() {
   var navbar = document.getElementById("myNavbar");
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     navbar.className = "w3-navbar" + " w3-card-2" + " w3-animate-top" + " w3-white";
   } else {
     navbar.className = navbar.className.replace(" w3-card-2 w3-animate-top w3-white", "");
   }
- }
- 
+}
